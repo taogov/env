1c484d7145321ddd5803f34a2b7aa3f80a6df0f8
wentao <taogov@gmail.com>
2018-06-14 17:56:29 +0800

    init

---
+++ code/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$redis = new Redis();
+
+$client = $redis->connect('redis', '6379');
+
+var_dump($client);
+
--- web/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-/etc/init.d/nginx start
-/etc/init.d/php7.0-fpm start
-
-tail -f /dev/null
